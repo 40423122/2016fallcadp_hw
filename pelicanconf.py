25291ec643412fd8fd679f16bc3e32f05be6da60
40423114 <40423114@gm.nfu.edu.tw>
2017-01-18 22:09:35 +0800

    40423122

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'KMOL'
+AUTHOR = '40423122'
 SITENAME = '2016Fall 電腦輔助設計實習 (虎尾科大MDE)'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
